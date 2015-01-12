b3fe61a5317904383eff48a8c78d975e14db0e4b
Hari haran <hariharan.uno@gmail.com>
2015-01-13 05:17:49 +0530

    Clean game.go

--- tron/game.go
+++ tron/game.go
@@ -12,26 +12,21 @@ import (
 type ID uint16
 
 type Game struct {
-	//settings
-	maxplayers, maxdeaths int
-	//game speed and respawn delay
-	speed, delay time.Duration
-	//total score+board size
-	w, h, bw, bh int
-	//ssh server
-	server *Server
-	//state
-	sidebar  *Sidebar
-	board    Board
-	idPool   chan ID
-	playerId ID
-	players  map[ID]*Player
-	log      func(string, ...interface{})
+	maxplayers, maxdeaths int           // settings
+	speed, delay          time.Duration // game speed and respawn delay
+	w, h, bw, bh          int           // total score+board size
+	server                *Server       // ssh server
+	sidebar               *Sidebar      // state
+	board                 Board
+	idPool                chan ID
+	playerId              ID
+	players               map[ID]*Player
+	logf                  func(format string, vars ...interface{})
 }
 
 func NewGame(port, width, height, maxplayers, maxdeaths int, speed, delay time.Duration) *Game {
 
-	//create an id pool
+	// create an id pool
 	idPool := make(chan ID, maxplayers)
 	for id := 1; id <= maxplayers; id++ {
 		idPool <- ID(id)
@@ -56,7 +51,7 @@ func NewGame(port, width, height, maxplayers, maxdeaths int, speed, delay time.D
 
 func (g *Game) Play() {
 
-	//build walls
+	// build walls
 	for w := 0; w < g.bw; w++ {
 		g.board[w][0] = wall
 		g.board[w][g.bh-1] = wall
@@ -66,21 +61,21 @@ func (g *Game) Play() {
 		g.board[g.bw-1][h] = wall
 	}
 
-	//start the game ticker!
+	// start the game ticker!
 	go g.tick()
 
-	//ready for players!
-	g.log("game started (#%d player slots)", len(g.idPool))
+	// ready for players!
+	g.logf("game started (#%d player slots)", len(g.idPool))
 
-	//watch signals (catch Ctrl+C and gracefully shutdown)
+	// watch signals (catch Ctrl+C and gracefully shutdown)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go g.watch(c)
 
-	//start the ssh server
+	// start the ssh server
 	go g.server.start()
 
-	//handle incoming players
+	// handle incoming players
 	for p := range g.server.newPlayers {
 		go g.handle(p)
 	}
@@ -91,21 +86,21 @@ func (g *Game) watch(c chan os.Signal) {
 	for _, p := range g.players {
 		p.teardown()
 	}
-	g.log("game ending...")
+	g.logf("game ending...")
 	time.Sleep(300 * time.Millisecond)
 	os.Exit(0)
 }
 
 func (g *Game) handle(p *Player) {
-	//connected with a valid id
-	//set game and id then play (blocking)
+	// connected with a valid id
+	// set game and id then play (blocking)
 	p.g = g
 	g.players[p.id] = p
 	p.play()
-	//disconnected
+	// disconnected
 	delete(g.players, p.id)
 	g.death(p)
-	//reinsert back into pool
+	// reinsert back into pool
 	g.idPool <- p.id
 }
 
@@ -114,10 +109,10 @@ func (g *Game) death(p *Player) {
 	p.deaths++
 	p.tdeath = time.Now()
 
-	//leave player on board for [delay]ms
+	// leave player on board for [delay]
 	time.Sleep(g.delay)
 
-	//clear!
+	// clear!
 	for w := 0; w < g.bw; w++ {
 		for h := 0; h < g.bh; h++ {
 			if g.board[w][h] == p.id {
@@ -128,22 +123,22 @@ func (g *Game) death(p *Player) {
 
 	p.waiting = false
 
-	//maximum deaths! kick!
+	// maximum deaths! kick!
 	if p.deaths == g.maxdeaths {
 		p.teardown()
 	}
 }
 
 func (g *Game) tick() {
-	//forever
+	// loop forever
 	for {
-		//move each player 1 square
+		// move each player 1 square
 		for _, p := range g.players {
-			//skip this player
+			// skip this player
 			if p.dead {
 				continue
 			}
-			//move player in [d]irection
+			// move player in [d]irection
 			p.d = p.nextd
 			switch p.d {
 			case dup:
@@ -155,35 +150,35 @@ func (g *Game) tick() {
 			case dright:
 				p.x++
 			}
-			//player is in a wall
+			// player is in a wall
 			if g.board[p.x][p.y] != blank {
-				//is it another player's wall? kills++
+				// is it another player's wall? kills++
 				id := g.board[p.x][p.y]
 				if other, ok := g.players[id]; ok && other != p {
 					other.kills++
 					other.log("killed %s", p.cname)
 				}
-				//this player dies...
+				// this player dies...
 				p.dead = true
 				go g.death(p)
 				continue
 			}
-			//place a player square
+			// place a player square
 			g.board[p.x][p.y] = p.id
 		}
 
-		//render the sidebar (and potentially flip the changed flag)
+		// render the sidebar (and potentially flip the changed flag)
 		g.sidebar.render()
 
-		//send delta updates to each player
+		// send delta updates to each player
 		for _, p := range g.players {
 			if p.ready {
 				p.update()
 			}
 		}
-		//mark update sent to all
+		// mark update sent to all
 		g.sidebar.changed = false
-		//sleep
+		// sleep
 		time.Sleep(g.speed)
 	}
 }
